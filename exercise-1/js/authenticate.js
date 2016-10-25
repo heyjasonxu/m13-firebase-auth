7e8cc4f096da1fafaeb5350f145852067b6c2b0f
Jason <yojasonxu@gmail.com>
2016-10-25 10:14:17 -0700

    udpate

--- exercise-1/js/authenticate.js
+++ exercise-1/js/authenticate.js
@@ -1,15 +1,32 @@
 // JavaScript authentication file
 $(function() {
 
-    // Initialize Firebase
+  // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyAM7UTSAxji2QnT-epmbK8aNGbAXAI_osg",
+        authDomain: "slick-62f7e.firebaseapp.com",
+        databaseURL: "https://slick-62f7e.firebaseio.com",
+        storageBucket: "slick-62f7e.appspot.com",
+        messagingSenderId: "1055455660051"
+    };
+    firebase.initializeApp(config);
 
     // Sign Up: Function to create account on firebase, then redirect to index.html
     var signUp = function() {
         // Get email, password, and display name
-
+        var email = $('#email').val();
+        var password = $('#password').val();
+        var displayName = $('display-name').val();
 
         // Create user, then set the user's display name
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(function(user){
+            user.updateProfile({
+                displayName: displayName,
+                photoURL: 'photo'
+            }).then(function() {
 
+            })
+        })
                 // Set display name
 
     };
@@ -17,9 +34,13 @@ $(function() {
     // SignIn: Function to authenticate on Firebase, then redirect to index.html
     var signIn = function() {
         // Get email and password
-
+        var email = $('email').val();
+        var password = $('password').val();
 
         // Authenticate using email and password, then redirect
+        firebase.auth().signInWithEmailAndPassword().then(){
+            
+        }
 
     };
 
